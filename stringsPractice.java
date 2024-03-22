255b47083cf18bcc8f344fa195ec7c22d6147c1f
shanukumar59720 <shanukumar59720>
2024-03-22 20:25:20 +0530

    string stringsPractice

---
+++ stringsPractice.java
@@ -0,0 +1,7 @@
+public class stringsPractice {
+    public static void main(String[] args) {
+        String s = "SACHIN TENDULKAR";
+        int len = s.trim().length();
+        System.out.println(len);        
+    }
+}
