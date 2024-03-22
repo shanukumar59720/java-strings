cc6511a6b0d6142f6f4776f2edcde0b8145c3895
shanukumar59720 <shanukumar59720>
2024-03-22 20:30:28 +0530

    stringsPractice

--- stringsPractice.java
+++ stringsPractice.java
@@ -1,7 +1,12 @@
 public class stringsPractice {
     public static void main(String[] args) {
-        String s = "SACHIN TENDULKAR";
-        int len = s.trim().length();
-        System.out.println(len);        
+        // String s = "SACHIN TENDULKAR";
+        // int len = s.trim().length();
+        //System.out.println(len);      
+        //System.out.println("A" == "A");  
+
+        String fName = "James";
+        String lName = "Gosling";
+        System.out.println(fName  = lName);
     }
 }
